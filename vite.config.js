6a0d4b41e7c6f09608de27b67305e7cf4d95c2ab
dip-ryota-kaneko1 <130530583+dip-ryota-kaneko1@users.noreply.github.com>
2024-03-02 18:31:48 +0900

    GitHub PagesにデプロイするためのワークフローとViteの設定を追加

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://dip-ryota-kaneko1.github.io/gha-handson/",
 })
